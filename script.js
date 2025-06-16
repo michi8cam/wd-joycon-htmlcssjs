06429371925d777334815e6d0381012f168ac370
michi8cam <misselianisaputra@gmail.com>
2025-06-16 20:13:04 +0700

    html css js

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function () {
+    particlesJS("particles-js", {
+    "particles": {
+        "number": {
+        "value": 80
+        },
+        "color": {
+        "value": "#00ffff"
+        },
+        "shape": {
+        "type": "circle"
+        },
+        "opacity": {
+        "value": 0.5
+        },
+        "size": {
+        "value": 3
+        },
+        "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#00ffff",
+        "opacity": 0.4,
+        "width": 1
+        },
+        "move": {
+        "enable": true,
+        "speed": 2
+        }
+    },
+   
+} )
